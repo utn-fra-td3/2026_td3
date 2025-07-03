b88bf8ac3a7c3bfbbf2be71cd4bb731b72039a29
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-07-03 01:35:15 -0300

    chore: elimino tarea de init

--- 4_workspace/freertos_task_handle/freertos_task_handle.c
+++ 4_workspace/freertos_task_handle/freertos_task_handle.c
@@ -13,24 +13,6 @@
 TaskHandle_t task_1, task_2;
 
 /**
- * @brief Tarea de inicializacion
- */
-void task_init(void *params) {
-    // Inicializacion de entradas
-    gpio_init(UP_BTN);
-    gpio_init(DW_BTN);
-    gpio_init(DEL_BTN);
-    gpio_set_dir(UP_BTN, false);
-    gpio_set_dir(DW_BTN, false);
-    gpio_set_dir(DEL_BTN, false);
-    gpio_pull_up(UP_BTN);
-    gpio_pull_up(DW_BTN);
-    gpio_pull_up(DEL_BTN);
-    // Elimino la tarea para liberar recursos
-    vTaskDelete(NULL);
-}
-
-/**
  * @brief Tarea que gestiona los botones
  */
 void task_btn(void *params) {
@@ -39,20 +21,22 @@ void task_btn(void *params) {
         // Chequeo los botones
         if(!gpio_get(UP_BTN)) {
             // Sube la prioridad de la tarea 1
-            vTaskPrioritySet(task_1, uxTaskPriorityGet(task_1) + 1);
+            int32_t prio = uxTaskPriorityGet(task_1);
+            vTaskPrioritySet(task_1, (prio + 1 < configMAX_PRIORITIES)? prio + 1 : prio);
         }
-        if(!gpio_get(DW_BTN)) {
+        else if(!gpio_get(DW_BTN)) {
             // Bajo la prioridad de la tarea 1
             int32_t prio = uxTaskPriorityGet(task_1);
             vTaskPrioritySet(task_1, (prio < 2)? 1 : prio - 1);
         }
-        if(!gpio_get(DEL_BTN)) {
-            // Elimina ambas tareas
+        else if(!gpio_get(DEL_BTN)) {
+            // Elimina ambas tareas y limpia los handle
             vTaskDelete(task_1);
             vTaskDelete(task_2);
+            puts("Tareas eliminadas");
         }
         // Bloqueo para dar espacio a otras tareas
-        vTaskDelay(pdMS_TO_TICKS(250));
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
@@ -67,6 +51,7 @@ void task_msg(void *params) {
 
     while(1) {
         printf("Tarea %d con prioridad %d\n", id, uxTaskPriorityGet(handle));
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
 
@@ -74,16 +59,27 @@ void task_msg(void *params) {
  * @brief Programa principal
  */
 int main(void) {
+
     stdio_init_all();
+
+    // Inicializacion de entradas
+    gpio_init(UP_BTN);
+    gpio_init(DW_BTN);
+    gpio_init(DEL_BTN);
+    gpio_set_dir(UP_BTN, false);
+    gpio_set_dir(DW_BTN, false);
+    gpio_set_dir(DEL_BTN, false);
+    gpio_pull_up(UP_BTN);
+    gpio_pull_up(DW_BTN);
+    gpio_pull_up(DEL_BTN);    
 
     const uint8_t id_1 = 1, id_2 = 2;
     // Creacion de tareas
-    xTaskCreate(task_init, "Init", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
-    xTaskCreate(task_btn, "Btn", configMINIMAL_STACK_SIZE, NULL, 4, NULL);
+    xTaskCreate(task_btn, "Btn", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
     xTaskCreate(task_msg, "Msg1", 2 * configMINIMAL_STACK_SIZE, (void*)&id_1, 2, &task_1);
     xTaskCreate(task_msg, "Msg2", 2 * configMINIMAL_STACK_SIZE, (void*)&id_2, 2, &task_2);
 
     // Incicia el sistema operativo
     vTaskStartScheduler();
-    while (1);
+    while(1);
 }
