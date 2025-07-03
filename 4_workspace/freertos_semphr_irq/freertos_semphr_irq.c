6c37a4d59463bf734b143adc16ac13c685903e8a
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-07-03 00:52:26 -0300

    chore: elimino tarea de init y doy soporte a picow

--- 4_workspace/freertos_semphr_irq/freertos_semphr_irq.c
+++ 4_workspace/freertos_semphr_irq/freertos_semphr_irq.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+// Inclusión condicional de la biblioteca del chip de WiFi
+#ifdef CYW43_WL_GPIO_LED_PIN
+#include "pico/cyw43_arch.h"
+#endif
+
 #include "FreeRTOS.h"
 #include "semphr.h"
 
@@ -22,26 +27,6 @@ void irq_callback(uint gpio, uint32_t event_mask) {
 }
 
 /**
- * @brief Tarea de inicializacion
- */
-void task_init(void *params) {
-    // Inicializacion de GPIO para pulsador
-    gpio_init(SEMPHR_BTN);
-    gpio_set_dir(SEMPHR_BTN, false);
-    gpio_pull_up(SEMPHR_BTN);
-    // Inicializo interrupcion para GPIO por flanco descendente
-    gpio_set_irq_enabled_with_callback(SEMPHR_BTN, GPIO_IRQ_EDGE_FALL, true, irq_callback);
-    // Inicializacion de GPIO para LED
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, true);
-    // Inicializacion de semaforo, tomo para bloquar
-    vSemaphoreCreateBinary(semphr);
-    xSemaphoreTake(semphr, portMAX_DELAY);
-    // Elimino tarea para liberar recursos
-    vTaskDelete(NULL);
-}
-
-/**
  * @brief Tarea que maneja el LED
  */
 void task_led(void *params) {
@@ -49,8 +34,12 @@ void task_led(void *params) {
     while(1) {
         // Intenta tomar el semaforo, se bloquea si no esta disponible
         xSemaphoreTake(semphr, portMAX_DELAY);
+    #ifdef CYW43_WL_GPIO_LED_PIN
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, !cyw43_arch_gpio_get(CYW43_WL_GPIO_LED_PIN));
+    #else
         // Conmuta el LED
         gpio_put(PICO_DEFAULT_LED_PIN, !gpio_get(PICO_DEFAULT_LED_PIN));
+    #endif
     }
 }
 
@@ -60,8 +49,26 @@ void task_led(void *params) {
 int main(void) {
     stdio_init_all();
 
+    // Inicializacion de GPIO para pulsador
+    gpio_init(SEMPHR_BTN);
+    gpio_set_dir(SEMPHR_BTN, false);
+    gpio_pull_up(SEMPHR_BTN);
+    // Inicializo interrupcion para GPIO por flanco descendente
+    gpio_set_irq_enabled_with_callback(SEMPHR_BTN, GPIO_IRQ_EDGE_FALL, true, irq_callback);
+
+#ifdef CYW43_WL_GPIO_LED_PIN
+    cyw43_arch_init();
+#else
+    // Inicializacion de GPIO para LED
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, true);
+#endif
+
+    // Inicializacion de semaforo, tomo para bloquar
+    vSemaphoreCreateBinary(semphr);
+    xSemaphoreTake(semphr, portMAX_DELAY);
+
     // Creacion de tareas
-    xTaskCreate(task_init, "Init", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
     xTaskCreate(task_led, "LED", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 
     // Inicia el sistema operativo
