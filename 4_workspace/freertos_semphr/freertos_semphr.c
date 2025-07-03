b6b852a17e7f2b79dd663ce19a07a583984c2c12
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-07-03 00:41:22 -0300

    chore: elimino tarea de init y doy soporte a picow

--- 4_workspace/freertos_semphr/freertos_semphr.c
+++ 4_workspace/freertos_semphr/freertos_semphr.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+// Inclusión de biblioteca del chip para Pico W
+#ifdef CYW43_WL_GPIO_LED_PIN
+#include "pico/cyw43_arch.h"
+#endif
+
 #include "FreeRTOS.h"
 #include "semphr.h"
 
@@ -9,24 +14,6 @@
 
 // Semaforo para desbloquear tarea
 SemaphoreHandle_t semphr;
-
-/**
- * @brief Tarea de inicializacion
- */
-void task_init(void *params) {
-    // Inicializacion de GPIO para pulsador
-    gpio_init(SEMPHR_BTN);
-    gpio_set_dir(SEMPHR_BTN, false);
-    gpio_pull_up(SEMPHR_BTN);
-    // Inicializacion de GPIO para LED
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, true);
-    // Inicializacion de semaforo y lo tomo para bloquar
-    vSemaphoreCreateBinary(semphr);
-    xSemaphoreTake(semphr, portMAX_DELAY);
-    // Elimino tarea para liberar recursos
-    vTaskDelete(NULL);
-}
 
 /**
  * @brief Tarea que maneja el semaforo
@@ -51,8 +38,14 @@ void task_led(void *params) {
     while(1) {
         // Intenta tomar el semaforo, se bloquea si no esta disponible
         xSemaphoreTake(semphr, portMAX_DELAY);
+    
+    #ifdef CYW43_WL_GPIO_LED_PIN
+        // Conmuta el LED desde el chip
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, !cyw43_arch_gpio_get(CYW43_WL_GPIO_LED_PIN));
+    #else
         // Conmuta el LED
         gpio_put(PICO_DEFAULT_LED_PIN, !gpio_get(PICO_DEFAULT_LED_PIN));
+    #endif
     }
 }
 
@@ -62,8 +55,25 @@ void task_led(void *params) {
 int main(void) {
     stdio_init_all();
 
+    // Inicializacion de GPIO para pulsador
+    gpio_init(SEMPHR_BTN);
+    gpio_set_dir(SEMPHR_BTN, false);
+    gpio_pull_up(SEMPHR_BTN);
+
+#ifdef CYW43_WL_GPIO_LED_PIN
+    // Inicialización del chip de WiFi
+    cyw43_arch_init();
+#else
+    // Inicializacion de GPIO para LED
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, true);
+#endif
+
+    // Inicializacion de semaforo y lo tomo para bloquar
+    vSemaphoreCreateBinary(semphr);
+    xSemaphoreTake(semphr, portMAX_DELAY);
+
     // Creacion de tareas
-    xTaskCreate(task_init, "Init", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
     xTaskCreate(task_semphr, "Semphr", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
     xTaskCreate(task_led, "LED", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 
