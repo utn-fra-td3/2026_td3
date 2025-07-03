f03876f430106cf5b208720a4c28730211748ab6
Fabrizio Carlassara <fabrizio.carlassara@gmail.com>
2025-07-03 00:05:08 -0300

    feat: soporte para picow

--- 4_workspace/freertos_blinky/freertos_blinky.c
+++ 4_workspace/freertos_blinky/freertos_blinky.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#ifdef CYW43_WL_GPIO_LED_PIN
+#include "pico/cyw43_arch.h"
+#endif
+
 #include "FreeRTOS.h"
 #include "task.h"
-
-/**
- * @brief Tarea de inicializacion
- */
-void task_init(void *params) {
-    // Inicializacion de GPIO
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, true);
-    gpio_put(PICO_DEFAULT_LED_PIN, 0);
-    // Elimino la tarea para liberar recursos
-    vTaskDelete(NULL);
-}
 
 /**
  * @brief Tarea de blinky de LED
@@ -22,10 +14,15 @@ void task_init(void *params) {
 void task_blinky(void *params) {
     
     while(1) {
-        // Toggle
+    #ifdef CYW43_WL_GPIO_LED_PIN
+        // Le pido al chip de WiFi que conmute el GPIO 
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, !cyw43_arch_gpio_get(CYW43_WL_GPIO_LED_PIN));
+    #else
+        // Toggle GPIO
         gpio_put(PICO_DEFAULT_LED_PIN, !gpio_get(PICO_DEFAULT_LED_PIN));
+    #endif
         // Demora de ticks equivalentes a 500 ms
-        vTaskDelay(pdMS_TO_TICKS(500));
+        vTaskDelay(pdMS_TO_TICKS(250));
     }
 }
 
@@ -35,8 +32,17 @@ void task_blinky(void *params) {
 int main(void) {
     stdio_init_all();
 
+#ifdef CYW43_WL_GPIO_LED_PIN
+    // Para Pico W, se habilita el chip de WiFi que controla el LED
+    cyw43_arch_init();
+#else
+    // Inicializacion de GPIO
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, true);
+    gpio_put(PICO_DEFAULT_LED_PIN, 0);
+#endif
+
     // Creacion de tareas
-    xTaskCreate(task_init, "Init", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
     xTaskCreate(task_blinky, "Blinky", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 
     // Arranca el scheduler
